b681822ecde6cb0d6ee209cfc224abc401e35858
Park JunHyoung <pj93251428@gmail.com>
2024-06-04 20:53:37 +0900

    feat : FindMainRooms

--- Assets/_Scripts/Dungeon/DungeonGenerator.cs
+++ Assets/_Scripts/Dungeon/DungeonGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Delaunay;
 using UnityEngine.UI;
+using System.Linq;
 
 /// <summary>
 /// Create Dungeon with Delaunay Triangulation(DT) + Minimum Spanning Tree(MST)
@@ -34,6 +35,9 @@ public class DungeonGenerator : MonoBehaviour
     const int PIXEL = 1;
     List<Room> rooms;
 
+    private HashSet<Vertex> points = new HashSet<Vertex>();
+    private List<(int index, Vector2 pos)> selectedRooms = new List<(int, Vector2)>();
+
     void Awake()
     {
         buttonGen.onClick.AddListener(GenerateDungeon);
@@ -44,7 +48,6 @@ public class DungeonGenerator : MonoBehaviour
     {
         // 물리 연산으로 각 방 퍼트리기, 코루틴
         StartCoroutine(SpreadRoomRoutine());
-
 
         // 방 중 Main 룸(일정 크기 이상의) 고르기
 
@@ -64,14 +67,15 @@ public class DungeonGenerator : MonoBehaviour
             rooms.Add(Instantiate(roomPrefab, GetRandomPointInCircle(randomPoint), Quaternion.identity, gridObject.transform));
             if ( i > selectRoomCnt )
             {
-                rooms [i].SetSize(GetRandomScale(smallMinRoomSize, smallMaxRoomSize));
+                rooms [i].transform.localScale = GetRandomScale(smallMinRoomSize, smallMaxRoomSize);
+                rooms [i].SetSize();
             }
             else
             {
-                rooms [i].SetSize(GetRandomScale(minRoomSize, maxRoomSize));
+                rooms [i].transform.localScale = GetRandomScale(minRoomSize, maxRoomSize);
+                rooms [i].SetSize();
             }
-            rooms [i].FillTile();
-            yield return null;
+            yield return new WaitForSeconds(0.05f);
         }
 
         Debug.Log("Wait for Physics Calculate");
@@ -83,7 +87,54 @@ public class DungeonGenerator : MonoBehaviour
 
         yield return null;
     }
+   
+    void FindMainRooms()
+    {
+        // 각 방의 크기, 비율, 인덱스를 저장할 리스트 생성
+        List<(float size, int index)> tmpRooms = new List<(float size, int index)>();
 
+        for ( int i = 0; i < rooms.Count; i++ )
+        {
+            rooms [i].GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            rooms [i].GetComponent<BoxCollider2D>().isTrigger = true;
+            rooms [i].transform.position = new Vector3(RoundPos(rooms [i].transform.position.x, PIXEL), RoundPos(rooms [i].transform.position.y, PIXEL), 1);
+
+
+            Vector3 scale = rooms [i].transform.localScale;
+            float size = scale.x * scale.y; // 방의 크기(넓이) 계산
+            float ratio = scale.x / scale.y; // 방의 비율 계산
+            if ( ratio > 2f || ratio < 0.5f ) continue; // 1:2 또는 2:1 비율을 초과하는 경우 건너뛰기
+            tmpRooms.Add((size, i));
+        }
+
+        // 방의 크기에 따라 내림차순으로 정렬
+        var sortedRooms = tmpRooms.OrderByDescending(room => room.size).ToList();
+
+        // 모든 방을 일단 비활성화
+        foreach ( var room in rooms )
+        {
+            room.gameObject.SetActive(false);
+        }
+
+        // 비율 조건을 만족하는 방 선택 및 처리
+        int count = 0;
+        selectedRooms = new List<(int, Vector2)>();
+        foreach ( var roomInfo in sortedRooms )
+        {
+            if ( count >= selectRoomCnt ) break; // 선택 후 종료
+            GameObject room = rooms [roomInfo.index].gameObject;
+            SpriteRenderer renderer = room.GetComponent<SpriteRenderer>();
+            if ( renderer != null )
+            {
+                renderer.color = Color.red;
+            }
+            room.SetActive(true);
+            points.Add(new Delaunay.Vertex(( int ) room.transform.position.x, ( int ) room.transform.position.y)); // points 리스트에 추가
+            selectedRooms.Add((roomInfo.index, new Vector2(( int ) room.transform.position.x, ( int ) room.transform.position.y)));
+            count++;
+        }
+
+    }
 
 
     /// <summary>
