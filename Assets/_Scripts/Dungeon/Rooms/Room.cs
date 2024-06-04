ea3f8d8bab2bf50e8abcbcd0d4c4d2b46187d7ee
Park JunHyoung <pj93251428@gmail.com>
2024-06-04 20:53:01 +0900

    test : Tilemap 채우는 방식은 일단 현 단계에선 보류

--- Assets/_Scripts/Dungeon/Rooms/Room.cs
+++ Assets/_Scripts/Dungeon/Rooms/Room.cs
@@ -1,43 +1,59 @@
 using System.Collections;
-using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Room : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Rigidbody2D rigidBody;
 
     [SerializeField] Tilemap tilemap;
     [SerializeField] TileBase tile;
 
+    [Header("Room Size")]
+    [SerializeField] int sizeX;
+    [SerializeField] int sizeY;
 
-    public void SetSize(Vector3 vector)
+
+    Vector3 size;
+    public void SetSize( )
     {
-        tilemap.size = new Vector3Int((int)vector.x, ( int ) vector.y);
+        size = transform.localScale;
+        sizeX = (int)size.x;
+        sizeY = (int)size.y;
+        tilemap.size = new Vector3Int(sizeX, sizeY);
     }
 
     public void FillTile()
     {
-        for ( int x = 0; x < tilemap.size.x; x++ )
-        {
-            for ( int y = 0; y < tilemap.size.y; y++ )
-            {
-                tilemap.SetTile(new Vector3Int(x, y, 0), tile);
-            }
-        }
+        //transform.localScale = Vector3.one;
+        transform.position = new Vector3((int)transform.position.x, (int)transform.position.y, transform.position.z);
+
+        Vector3Int pos = tilemap.LocalToCell(transform.position);
+        Vector3Int startPos = tilemap.LocalToCell( new Vector3Int((pos.x - sizeX/2 ), (pos.y- sizeY/2)));
+        Vector3Int endPos = tilemap.LocalToCell(new Vector3Int(( pos.x + sizeX / 2 ), ( pos.y + sizeY / 2 )));
+        Debug.Log($"TilemapSize {tilemap.size}");
+        Debug.Log($"Pos : {pos} / StartPos {startPos}, EndPos{endPos}");
+        TilemapGenerator.Instance.FillTile(startPos, endPos, tile);
+
+        //tilemap.BoxFill(startPos, tile, startPos.x, startPos.y, endPos.x, endPos.y);
+        //tilemap.FloodFill(tilemap.LocalToCell(transform.position), tile);
     }
 
-    public void ActivateRigidBody( float waitTime)
+    public void ActivateRigidBody( float waitTime )
     {
         StartCoroutine(ActiveRigidBodyRoutine(waitTime));
     }
 
-    IEnumerator ActiveRigidBodyRoutine(float waitTime )
+    IEnumerator ActiveRigidBodyRoutine( float waitTime )
     {
-        rb.bodyType = RigidbodyType2D.Dynamic;
-        rb.gravityScale = 0f;
+        rigidBody.bodyType = RigidbodyType2D.Dynamic;
+        rigidBody.gravityScale = 0f;
         yield return new WaitForSeconds(waitTime);
-        Destroy(rb);
+        Destroy(rigidBody);
+
+        //FillTile();
+        //Destroy(gameObject);
     }
 
 }
--- Assets/_Scripts/Dungeon/TilemapGenerator.cs
+++ Assets/_Scripts/Dungeon/TilemapGenerator.cs
@@ -1,18 +1,46 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TilemapGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static TilemapGenerator instance;
+    public static TilemapGenerator Instance
     {
-        
+        get { return instance; }  }
+
+    [SerializeField] Tilemap baseMap;
+
+
+    private void Awake()
+    {
+        CreateInstance();
+        baseMap = GetComponent<Tilemap>();
+    }
+    
+    void CreateInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void FillTile(Vector3Int startPos, Vector3Int endPos, TileBase tile )
     {
-        
+        for(int x =  startPos.x; x < endPos.x; x++)
+        {
+            for(int y =  startPos.y; y < endPos.y; y++)
+            {
+                baseMap.SetTile(new Vector3Int(x, y), tile);
+            }
+        }
     }
 }
