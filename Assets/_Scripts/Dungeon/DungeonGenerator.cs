1eb39ec7f68fd853956ccfcf94b657c66e1131e4
Park JunHyoung <pj93251428@gmail.com>
2024-06-05 17:45:09 +0900

    feat : Draw Door and Corridor , have to Fix more

--- Assets/_Scripts/Dungeon/DungeonGenerator.cs
+++ Assets/_Scripts/Dungeon/DungeonGenerator.cs
@@ -24,8 +24,10 @@ public class DungeonGenerator : MonoBehaviour
     [Header("Prefabs")]
     [SerializeField]  protected GameObject gridObject; // tilemap Grid
     [SerializeField]  Room roomPrefab; // room
-    [SerializeField]  LineRenderer lineRenderer; //for Visualize 
+    [SerializeField]  LineRenderer graphRenderer; //for Visualize 
 
+    [SerializeField]  GameObject doorPrefab; // for test
+    [SerializeField]  LineRenderer corridor;
 
     [Header("Room Settings")]
     [SerializeField] int roomLayer;
@@ -40,6 +42,9 @@ public class DungeonGenerator : MonoBehaviour
     [SerializeField] protected int smallMinRoomSize;
     [SerializeField] protected int smallMaxRoomSize;
     [SerializeField] protected int overlapOffset;
+
+
+    [SerializeField] int offSet; // 복도 생성시 보정에 들어갈 offSet
 
     [Space(10)]
     [Range(0,100)]
@@ -102,15 +107,17 @@ public class DungeonGenerator : MonoBehaviour
         StartCoroutine(SpreadRoomRoutine());
 
         // 방 중 Main 룸(일정 크기 이상의) 고르기
-        //FindMainRooms();
+        //FindMainRooms(); V
 
         // 룸 정보를 2차원 배열에 추가
-        //GenerateMapArray();
+        //GenerateMapArray(); V
 
         // DT + MST 이용해서 룸 연결 (그래프)
-       // ConnectRooms();
+        // ConnectRooms(); V
 
-        // 복도 그려서 연결
+        // 복도 그려서 연결 
+
+        // 너무 긴 복도의 경우? 중간에 추가적으로 방 생성 ...
 
         // 얻어진 데이터를 Tilemap에 다시 그려냄 -> 데이터는 DungeonManager에 넘겨줘서 이후 작업들은 거기서 제어할것
     }
@@ -202,12 +209,12 @@ public class DungeonGenerator : MonoBehaviour
         {
             // rooms [i].GetComponent<BoxCollider2D>().isTrigger = true;
             rooms [i].transform.position = new Vector3(RoundPos(rooms [i].transform.position.x, PIXEL), RoundPos(rooms [i].transform.position.y, PIXEL), 1); //정수 단위로 변환
-            Debug.Log($"Translate to int : {rooms [i].transform.position}");
+            //Debug.Log($"Translate to int : {rooms [i].transform.position}");
 
             Vector3 scale = rooms [i].transform.localScale;
             float size = scale.x * scale.y; // 방의 크기(넓이) 계산
             float ratio = scale.x / scale.y; // 방의 비율 계산
-            Debug.Log($"Calculate Size & ratiod :  {size} ,  {ratio}");
+            // Debug.Log($"Calculate Size & ratiod :  {size} ,  {ratio}");
 
             if ( ratio > 2f || ratio < 0.5f ) continue; // 1:2 또는 2:1 비율을 초과하는 경우 건너뛰기, (제외하는 과정)
 
@@ -247,7 +254,7 @@ public class DungeonGenerator : MonoBehaviour
         GenerateMapArray();
     }
 
-
+        
 
     private void GenerateMapArray()
     {
@@ -308,33 +315,83 @@ public class DungeonGenerator : MonoBehaviour
         foreach(var edge in hallwayEdges )
         {
             yield return new WaitForSeconds(0.01f);
-            DrawLine(edge);
+            DrawLine(graphRenderer ,edge);
         }
 
-        StartCoroutine(GenerateHall(hallwayEdges));
+        StartCoroutine(GenerateHallways(hallwayEdges));
         yield return null;
     }
 
 
-    IEnumerator GenerateHall(IEnumerable <Edge> tree)
+    IEnumerator GenerateHallways(IEnumerable <Edge> tree)
     {
+        Debug.Log("Hallways Start");
+        // edge 순회해서 양 끝 지점에 현관, 복도 생성하는 메서드
         
+        foreach ( var edge in tree )
+        {
+            //양 끝 지점에 현관(문) 생성
+            GenerateDoor(edge); 
 
+            //복도생성은 여기에
+            GenerateCorrider(edge);
+        }
+
+        // 복도? 문 데이터 추가?
 
         yield return null;
     }
+    
+    // 문 생성 ( 임시 )
+    // 현재 방 중앙에 생성되는데, 방 사이즈 정보 받아서 끝에 생성할것
+    // 또는 복도를 먼저 생성하고 그 양끝에 문을 생성?
+    void GenerateDoor(Edge edge)
+    {
+        Vector3 startPos = new Vector3(edge.a.x, edge.a.y);
+        Vector3 endPos = new Vector3(edge.b.x, edge.b.y);
+        Instantiate(doorPrefab , startPos, Quaternion.identity,transform);
+        Instantiate(doorPrefab, endPos, Quaternion.identity, transform);
+    }
 
+    // x,y 좌표 검사해서 수직인지, 수평인지, ㄱ 인지, ㄴ 인지 등 Shape 결정 해서 복도 생성
+    void GenerateCorrider(Edge edge)
+    {
+        Vertex a = edge.a;
+        Vertex b = edge.b;
+
+        if ( Mathf.Abs(a.y - b.y) < offSet ) // 수평인 경우 ,offset 만큼은 보정해서 생성해줘야함
+        {
+            Vertex newA = new Vertex(a.x + offSet/2, a.y); //+ 할지 - 할지 있다가 수정
+            Vertex newB = new Vertex(b.x - offSet / 2, b.y);
+            Edge newEdge = new Edge(newA, newB);
+            DrawLine(corridor ,newEdge);
+        }
+        else if ( Mathf.Abs(a.x - b.x)  < offSet) // 수직인 경우
+        {
+            Vertex newA = new Vertex(a.x , a.y + offSet / 2); //+ 할지 - 할지 있다가 수정
+            Vertex newB = new Vertex(b.x, b.y - offSet / 2);
+            Edge newEdge = new Edge(newA, newB);
+            DrawLine(corridor, newEdge);
+        }
+        else //offset 벗어나는 경우 ㄱ , ㄴ 꺾이는 복도 생성
+        {
+            //  사실상 복도 두개 
+
+            // 꺾이는 방향? 결정은?
+        }
+
+    }
 
   
 
-    void DrawLine(Edge edge)
+    void DrawLine(LineRenderer line , Edge edge)
     {
-        LineRenderer line = Instantiate(lineRenderer,transform);
+        LineRenderer newLine = Instantiate(line,transform);
         Vector3 startPos = new Vector3(edge.a.x, edge.a.y, 0);
         Vector3 endPos = new Vector3(edge.b.x, edge.b.y, 0);
 
-        line.SetPosition(0, startPos);
-        line.SetPosition(1, endPos);
+        newLine.SetPosition(0, startPos);
+        newLine.SetPosition(1, endPos);
     }
 
     /// <summary>
